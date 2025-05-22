4b1bf8e14a602430b43e09588c85295eef19a03f
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-05-22 17:11:25 +0200

    add constructor to Zombie class for name initialization

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,12 +6,14 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 21:42:01 by azerfaou          #+#    #+#             */
-/*   Updated: 2025/05/20 21:42:02 by azerfaou         ###   ########.fr       */
+/*   Updated: 2025/05/22 17:10:46 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
+
+Zombie::Zombie(std::string name) : _name(name) {};
 
 void Zombie::addName(std::string newName)
 {
